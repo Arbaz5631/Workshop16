905bc033f4445fa03bdcf04310f8db600944733d
Arbaz khan <arbaz2279khan@gmail.com>
2021-06-06 22:08:12 +0530

    add api to get the data

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const fetch=require("node-fetch");
-
+const fetch=require("node-fetch")
+const express=require("express")
+const app=express();
+app.get("/data",async(req,res)=>{
+    const data=await getWeatherInfo();
+    console.log("data -->",data)
+    res.status(200).send({"data":data})
+})
 async function getWeatherInfo(){
-    const weather=await fetch(
+    const weatherData=await fetch(
         "https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric&APPID=0e12ad84dbe4bcdf1f980898cfc713b6"
 
     ).then((res)=>res.json()).catch(e=>console.log(e))
-    console.log(weather);    
+    console.log(weatherData)
+    return weatherData;
+   
 }
-
-getWeatherInfo();
+app.listen(9000,console.log("run on port 9000"))
